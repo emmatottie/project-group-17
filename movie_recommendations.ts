8d274adc683fefc0b6c076ea99df58463d040315
emmatottie <144359517+emmatottie@users.noreply.github.com>
2024-02-15 13:15:04 +0100

    Add files via upload

--- movie_recommendations.ts
+++ movie_recommendations.ts
@@ -1,11 +1,20 @@
-//oj vad kul med prpjekt
+//hejsan
 
-/*
-function movie_recommendation(movie, i) {
-    if (movie === api) {
-        search(title)
+function display_menu() {
+    console.log('Welcome to our Movie Recommendation System');
+    console.log('1. Search for a movie');
+    console.log('2. Exit');
+  }
+
+  // Function to handle user input
+function handle_input(option: string) {
+    let input = ""
+    if (input === "1"){
+        searchMovie()
+    } else if (input === "2") {
+        
+    } else {
+        console.log('Invalid option. Please try again.');
+        
     }
 }
-*/
-
-//hejsan
